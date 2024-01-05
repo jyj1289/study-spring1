a98ec2ab422b634878bbb01e9fc275cabf027eb8
jyj1289 <jin37091289@gmail.com>
2024-01-05 13:07:00 +0900

    ADD :: 게시글 전체 조회 서비스

--- src/main/java/com/yujin/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/yujin/freelecspringboot2webservice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.yujin.freelecspringboot2webservice.service.posts;
 
 import com.yujin.freelecspringboot2webservice.domain.posts.Posts;
 import com.yujin.freelecspringboot2webservice.domain.posts.PostsRepository;
+import com.yujin.freelecspringboot2webservice.web.dto.PostsListResponseDto;
 import com.yujin.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import com.yujin.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import com.yujin.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
