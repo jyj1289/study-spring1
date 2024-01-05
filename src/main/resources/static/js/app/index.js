2ba7bfb48edfab10e4ced4c946167496e11d9ba1
jyj1289 <jin37091289@gmail.com>
2024-01-05 13:33:48 +0900

    ADD :: update 이벤트 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,10 @@ var main = {
         $('#btn-save').on('click', function(){
             _this.save();
         });
+
+        $('#btn-update').on('click', function(){ // btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트를 등록
+            _this.update();
+        })
     },
     save : function(){
         var data = {
@@ -24,7 +28,28 @@ var main = {
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
+    },
+    update : function(){    // 신규로 추가될 update function
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT', // 여러 HTTP Method 중 PUT 메서드를 선택한다. PostApicontroller에 있는 API에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다. 이는 REST 규약에 맞게 설정된 것이다.
+            url: '/api/v1/posts/'+id,   // 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가한다.
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (){
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
     }
-}
+};
 
 main.init();
