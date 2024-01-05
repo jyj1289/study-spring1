3b02e3247763ed6d283de6d4fa4716ba82972908
jyj1289 <jin37091289@gmail.com>
2024-01-05 13:34:14 +0900

    ADD :: update method

--- src/main/java/com/yujin/freelecspringboot2webservice/web/IndexController.java
+++ src/main/java/com/yujin/freelecspringboot2webservice/web/IndexController.java
@@ -1,10 +1,19 @@
 package com.yujin.freelecspringboot2webservice.web;
 
+import com.yujin.freelecspringboot2webservice.domain.posts.Posts;
+import com.yujin.freelecspringboot2webservice.service.posts.PostsService;
+import com.yujin.freelecspringboot2webservice.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
 
     @GetMapping("/")
     public String index(){
@@ -14,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
