b494b7d12e73279e576a5617da6b487c24046f1e
jyj1289 <jin37091289@gmail.com>
2024-01-11 17:06:25 +0900

    ADD :: 삭제 컨트롤러 추가

--- src/main/java/com/yujin/freelecspringboot2webservice/web/PostsApiController.java
+++ src/main/java/com/yujin/freelecspringboot2webservice/web/PostsApiController.java
@@ -36,4 +36,10 @@ public class PostsApiController {
         return postsService.findAllDesc();
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
 }
