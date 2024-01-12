dbe4be26b63aca605fca77dffd788e2a8234c659
jyj1289 <jin37091289@gmail.com>
2024-01-12 17:32:36 +0900

    FIX :: SessionUser 를 import 해야되는데 Session 을 가져와서 parameter 와 비교하는 오류를 검출함

--- src/main/java/com/yujin/freelecspringboot2webservice/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/yujin/freelecspringboot2webservice/config/auth/LoginUserArgumentResolver.java
@@ -2,6 +2,7 @@ package com.yujin.freelecspringboot2webservice.config.auth;
 // Login-UserArgumentResolver 라는 HandlerMethodArgumentResolver 인터페이스를 구현한 클래스이다.
 // HandlerMethodArgumentResolver 는 조건에 맞는 경우 메소드가 있다면 HandlerMethodArgumentResolver 의 구현체가 지정한 값으로 해당 메소드의 파라미터로 넘길 수 있다.
 
+import com.yujin.freelecspringboot2webservice.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,6 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -20,9 +20,9 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     private final HttpSession httpSession;
 
     @Override
-    public boolean supportsParameter(MethodParameter parameter){    // 컨트롤러 메섣의 특정 파라미터를 지원하는지 판단한다. 여기서는 파라미터에 @LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class인 경우 true를 반환한다.
+    public boolean supportsParameter(MethodParameter parameter){    // 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단한다. 여기서는 파라미터에 @LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class인 경우 true를 반환한다.
         boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
-        boolean isUserClass = Session.class.equals(parameter.getParameterType());
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
         return isLoginUserAnnotation && isUserClass;
     }
 
