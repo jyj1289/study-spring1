c2562ba5b3742613871ef16daec88ab7d380713d
jyj1289 <jin37091289@gmail.com>
2024-01-13 18:37:59 +0900

    FIX :: JPA Auditing 제거

--- src/main/java/com/yujin/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
+++ src/main/java/com/yujin/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+// @EnableJpaAuditing 가 삭제됨
+// @EnableJpaAuditing  // JPA Auditing 활성화
 @SpringBootApplication
 public class FreelecSpringboot2WebserviceApplication {
 
